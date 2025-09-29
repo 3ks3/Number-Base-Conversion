75bacc2d163f9529e4b90c916724fa4e75c6b31f
3ks3 <kaydensilva10@gmail.com>
2025-09-28 22:17:44 -0400

    Update convert.c

--- convert.c
+++ convert.c
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 void div_convert(uint32_t n, int base, char *out)
 {
@@ -67,6 +68,128 @@ void sub_convert(uint32_t n, int base, char *out)
     strcpy(out, result);
 }
 
+void oct_to_bin(const char *oct, char *out)
+{
+    int pos = 0;
+
+    for (int i = 0; oct[i] != '\0'; i ++)
+    {
+        int digit = oct[i] - '0';
+
+        out[pos++] = (digit & 4) ? '1' : '0';
+        out[pos++] = (digit & 2) ? '1' : '0';
+        out[pos++] = (digit & 1) ? '1' : '0';
+    }
+    out[pos] = '\0';
+}
+
+void oct_to_hex(const char *oct, char *out)
+{
+    char bin[100] = {0};
+    oct_to_bin(oct, bin);
+
+    int len = strlen(bin);
+
+    int padded_len = len;
+
+    if (len % 4 != 0)
+    {
+        padded_len = len + (4 - (len % 4));
+    }
+
+    char padded_bin[100] = {0};
+
+    int pad_zeros = padded_len - len;
+
+    for (int i = 0; i < pad_zeros; i++)
+    {
+        padded_bin[i] = '0';
+    }
+
+    strcpy(padded_bin + pad_zeros, bin);
+
+    int out_pos = 0;
+
+    for (int i = 0; i < padded_len; i += 4)
+    {
+        int val = 0;
+
+        for (int j = 0; j < 4; j++)
+        {
+            val = (val << 1) | (padded_bin[i + j] - '0');
+        }
+        out[out_pos++] = "0123456789ABCDEF"[val];
+    }
+    out[out_pos] = '\0';
+}
+
+void hex_to_bin(const char *hex, char *out)
+{
+    int pos = 0;
+
+    for (int i = 0; hex[i] != '\0'; i++)
+    {
+        char c = hex[i];
+        int val;
+
+        if (isdigit(c)) val = c - '0';
+        else if (c >= 'A' && c <= 'F') val = c - 'A' + 10;
+        else if (c >= 'a' && c <= 'f') val = c - 'a' + 10;
+        else continue;
+
+        out[pos++] = (val & 8) ? '1' : '0';
+        out[pos++] = (val & 4) ? '1' : '0';
+        out[pos++] = (val & 2) ? '1' : '0';
+        out[pos++] = (val & 1) ? '1' : '0';
+    }
+    out[pos] = '\0';
+}
+
+void to_sign_magnitude(int32_t n, char *out)
+{
+    uint32_t val = (n >= 0) ? n : -n;
+
+    for (int i = 0; i < 32; i++)
+    {
+        out[i] = (val & (1U << (31 - i))) ? '1' : '0';
+    }
+    if (n < 0)
+    {
+        out[0] = '1';
+    }
+    out[32] = '\0';
+}
+
+void to_ones_complement(int32_t n, char *out)
+{
+    uint32_t val;
+
+    if (n >= 0)
+    {
+        val = n;
+    }
+    else
+    {
+        val = ~((uint32_t)(-n));
+    }
+    for (int i = 0; i < 32; i++)
+    {
+        out[i] = (val & (1U << (31 - i))) ? '1' : '0';
+    }
+    out[32] = '\0';
+}
+
+void to_twos_complement(int32_t n, char *out)
+{
+    uint32_t val = (uint32_t)n;
+
+    for (int i = 0; i < 32; i++)
+    {
+        out[i] = (val & (1U << (31 - i))) ? '1' : '0';
+    }
+    out[32] = '\0';
+}
+
 void print_tables(uint32_t n)
 {
     char bin[65], oct[65], hex[65];
@@ -93,5 +216,6 @@ void print_tables(uint32_t n)
 
     printf("AND with 0xFF: Binary=%s Octal=%s Decimal=%u Hex=%s\n", bin, oct, masked, hex);
 }
+
 
 
